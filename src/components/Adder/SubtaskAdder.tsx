a6364dacd32e3237bd8b46449f363f6a648bc57f
Yigit Ak <ak.yigitmesut@gmail.com>
2025-06-19 15:03:43 +0300

    SubtaskAdder done

--- src/components/Adder/SubtaskAdder.tsx
+++ src/components/Adder/SubtaskAdder.tsx
@@ -1,21 +1,45 @@
 import Adder from "./Adder.tsx";
 import Card, {MainContent} from "../layout/Card";
 import Checkbox from "../Checkbox";
+import type {CreateSubtaskDto} from "../../types/subtask.ts";
+import {FormEvent, useState} from "react";
+import type {CreateTaskDto} from "../../types/task.ts";
 
 interface Props {
-  add() : void;
+  add(subtask: CreateSubtaskDto): void;
 }
 
-export default function SubtaskAdder() {
+export default function SubtaskAdder({add}: Props) {
+  const [title, setTitle] = useState<string>("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const trimmed = title.trim();
+    if (!trimmed) return; // ignore empty submissions
+
+    const task: CreateSubtaskDto = {
+      title: trimmed,
+    };
+
+    add(task);
+  }
+
   return (
       <Adder title="Add subtask">
-        <Card className="adder">
-          <MainContent>
-            <Checkbox/>
-            <input type="text" autoFocus={true}/>
-            {/*<input name="parentTaskId" value={parentTaskId} hidden/> todo think how to implement*/}
-          </MainContent>
-        </Card>
+        <form onSubmit={handleSubmit}>
+          <Card className="adder">
+            <MainContent>
+              <Checkbox/>
+              <input type="text"
+                     autoFocus
+                     name="title"
+                     placeholder="Subtask title"
+                     value={title}
+                     onChange={(e) => setTitle(e.target.value)}
+              />
+            </MainContent>
+          </Card>
+        </form>
       </Adder>
   );
 }
